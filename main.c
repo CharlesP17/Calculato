64e8889b8c3d15f54ca3e8120934eec5a64f6f9c
CharlesP17 <charles.paradot@viacesi.fr>
2016-11-04 10:35:17 +0100

    Compute addition of two integers

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int soustraction (int c, int d)
+{
+    return c-d;
+}
 int addition( int a, int b )
 {
     return a+b;
@@ -9,7 +13,8 @@ int main ()
 {
     int result;
     result = addition (1,4);
-    printf("resultat de l'addition est = %d", result);
-
+    printf("resultat de l'addition est = %d\n", result);
+    result = soustraction (1,4);
+    printf("resultat de la soustraction est %d\n", result);
     return 0;
 }
